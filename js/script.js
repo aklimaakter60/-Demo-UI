6c55dab39e0f83a132e356c6630b59aea02b83a0
aklimaakter <aklimaaku123@gmail.com>
2025-07-19 18:02:02 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,141 @@
+$( function() {
+    var availableTags = [
+      "Afganistan",
+      "Algeria",
+      "Argentina",
+      "Bangladesh",
+      "Canada",
+      "China",
+      "Columbia",
+      "Croatia",
+      "Denmark",
+      "England",
+      "France",
+      "Greece",
+      "Hong Kong",
+      "Japan",
+      "Kenya",
+      "Malaysia",
+      "Nepal",
+      "Poland",
+      "Portugal",
+      "Russia",
+      "Spain",
+      "Sri Lanka"
+    ];
+    $( "#c_name" ).autocomplete({
+      source: availableTags
+    });
+  } );
+
+  $( function() {
+    $( "#accordion" ).accordion({
+      collapsible: true
+    });
+  } );
+
+   $( function() {
+    $( "#draggable" ).draggable();
+  } );
+
+
+   $( function() {
+    $( "#drag" ).draggable();
+    $( "#droppable" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "Dropped!" );
+      }
+    });
+  } );
+
+
+$( function() {
+    $( "#selectable" ).selectable();
+  } );
+
+
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+
+$( function() {
+    var progressbar = $( "#progressbar" ),
+      progressLabel = $( ".progress-label" );
+ 
+    progressbar.progressbar({
+      value: false,
+      change: function() {
+        progressLabel.text( progressbar.progressbar( "value" ) + "%" );
+      },
+      complete: function() {
+        progressLabel.text( "Complete!" );
+      }
+    });
+ 
+    function progress() {
+      var val = progressbar.progressbar( "value" ) || 0;
+ 
+      progressbar.progressbar( "value", val + 2 );
+ 
+      if ( val < 99 ) {
+        setTimeout( progress, 80 );
+      }
+    }
+ 
+    setTimeout( progress, 2000 );
+  } );
+
+//select menu
+$( function() {
+    $( "#speed" ).selectmenu();
+ 
+    $( "#files" ).selectmenu();
+ 
+    $( "#number" )
+      .selectmenu()
+      .selectmenu( "menuWidget" )
+        .addClass( "overflow" );
+ 
+    $( "#salutation" ).selectmenu();
+  } );
+
+//color picker
+$( function() {
+    function hexFromRGB(r, g, b) {
+      var hex = [
+        r.toString( 16 ),
+        g.toString( 16 ),
+        b.toString( 16 )
+      ];
+      $.each( hex, function( nr, val ) {
+        if ( val.length === 1 ) {
+          hex[ nr ] = "0" + val;
+        }
+      });
+      return hex.join( "" ).toUpperCase();
+    }
+    function refreshSwatch() {
+      var red = $( "#red" ).slider( "value" ),
+        green = $( "#green" ).slider( "value" ),
+        blue = $( "#blue" ).slider( "value" ),
+        hex = hexFromRGB( red, green, blue );
+      $( "#swatch" ).css( "background-color", "#" + hex );
+    }
+ 
+    $( "#red, #green, #blue" ).slider({
+      orientation: "horizontal",
+      range: "min",
+      max: 255,
+      value: 127,
+      slide: refreshSwatch,
+      change: refreshSwatch
+    });
+    $( "#red" ).slider( "value", 255 );
+    $( "#green" ).slider( "value", 140 );
+    $( "#blue" ).slider( "value", 60 );
+  } );
+
+
